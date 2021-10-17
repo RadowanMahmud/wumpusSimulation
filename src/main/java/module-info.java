79bdf8193d7658c5f5d049e02d3ed5a781f4babf
Ibrahim Khalil <mik858692@gmail.com>
2021-10-18 01:06:34 +0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.wumpusworldsimulation {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.wumpusworldsimulation to javafx.fxml;
     exports com.example.wumpusworldsimulation;
 }
